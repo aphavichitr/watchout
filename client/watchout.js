bbbba1ee8db088c13a3d8112150477bcd68ede57
aphavichitr <andrew.phavichitr@gmail.com>
2016-04-29 20:05:29 -0700

    Add enemies with random movements

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,85 @@
 // start slingin' some d3 here.
+var gameOptions = {
+  height: 450,
+  width: 700,
+  enemies: 30,
+  padding: 20
+};
+
+var gameStats = {
+  score: 0,
+  bestScore: 0
+};
+
+var axes = {  
+  x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
+  y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
+};
+
+
+var gameBoard = d3.select('.board')
+                  .append('svg')
+                  .attr({'class': 'svgBoard',
+                         'width': gameOptions.width,
+                         'height': gameOptions.height,
+                       })
+                  .style('background-color', 'orange');
+
+var updateScore = function() {
+  d3.select('current')
+    .text(gameStats.score.toString());
+};
+
+var updateBestScore = function() {
+  gameStats.bestScore = Math.max(gameStats.bestScore, gameStats.score);
+  d3.select('highscore').text(gameStats.bestScore.toString());
+};
+
+var numRandomCircle = function(n, r) {
+  var data = [];
+  var count = 0;
+  while (count < n) {
+    var prop = {
+      id: count,
+      cx: Math.random() * gameOptions.width,
+      cy: Math.random() * gameOptions.height,
+      r: r
+    };
+    data.push(prop);
+    count++;
+  }
+  return data;
+};
+
+var dataSet = numRandomCircle(20, 10);
+
+var circles = gameBoard.selectAll('.svgBoard')
+                .data(dataSet)
+                .enter()
+                .append('circle')
+                .attr({'class': 'svgCircle',
+                       'r': function(d) { return d.r; },
+                       'fill': 'blue',
+                       'cx': function(d) { return d.cx; },
+                       'cy': function(d) { return d.cy; }
+                     });
+
+var update = function(data) {
+  circles.transition()
+         .duration(1000)
+         .attr({'cx': function(d) { return (d.cx = Math.random() * gameOptions.width); },
+                'cy': function(d) { return (d.cy = Math.random() * gameOptions.height); }
+              });
+
+};
+
+
+setInterval(function() {
+  update(dataSet);
+  console.log('hi');
+}, 1000);
+  
+// d3.select('.mouse')
+//   .data(['blue'])
+//   .enter()
+//   .append('svg')
